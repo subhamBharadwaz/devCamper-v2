23a79b99790152da61abf415bfcb44dbfe1e40f8
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-10-10 10:26:29 +0530

    :heavy_plus_sign: chore(Setup morgan for logging)

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const dotEnv = require("dotenv").config({ path: "./config/config.env" });
+const morgan = require("morgan");
 
 // Route files
 const bootcamps = require("./routes/bootcamps");
 
 const app = express();
+
+// Dev logging middleware
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
 
 // Mount routers
 app.use("/api/v2/bootcamps", bootcamps);

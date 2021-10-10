465b524709d6159fd057f28a0eeaf18c9b6835cd
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-10-10 12:04:58 +0530

    :card_file_box: chore(mongoose setup)
    - Setup mongoose connection with atlas
    
    - Create an unhandled promise rejections function so that no need to
      wrap try catch

--- server.js
+++ server.js
@@ -1,6 +1,10 @@
 const express = require("express");
 const dotEnv = require("dotenv").config({ path: "./config/config.env" });
 const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+// Database connection
+connectDB();
 
 // Route files
 const bootcamps = require("./routes/bootcamps");
@@ -17,7 +21,15 @@ app.use("/api/v2/bootcamps", bootcamps);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+// Handle unhandled promise rejections
+process.on("unhandledRejection", (err, promise) => {
+  console.error(`Error: ${err.message}`);
+
+  // Close server and exit process
+  server.close(() => process.exit(1)); //exit(1) = exit with failure
+});

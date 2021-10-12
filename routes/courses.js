662e2e571bab27f806903623618c33f0f422ca3e
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-10-12 09:27:29 +0530

    :card_file_box: db(Create course for specific bootcamp)
    - Get a single course associated with a bootcamp
    - Create course for bootcamp

--- routes/courses.js
+++ routes/courses.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { getCourses } = require("../controller/courses");
+const { getCourses, getCourse, addCourse } = require("../controller/courses");
 
 const router = express.Router({ mergeParams: true }); // We are merging the URL param
 
-router.route("/").get(getCourses);
+router.route("/").get(getCourses).post(addCourse);
+router.route("/:id").get(getCourse);
 
 module.exports = router;

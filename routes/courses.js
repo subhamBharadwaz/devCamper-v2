cf067da27c816d6cd8517b451e21483ffc457f3d
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-10-12 09:38:47 +0530

    :building_construction: db(Update and Delete Course)

--- routes/courses.js
+++ routes/courses.js
@@ -1,9 +1,15 @@
 const express = require("express");
-const { getCourses, getCourse, addCourse } = require("../controller/courses");
+const {
+  getCourses,
+  getCourse,
+  addCourse,
+  updateCourse,
+  deleteCourse,
+} = require("../controller/courses");
 
 const router = express.Router({ mergeParams: true }); // We are merging the URL param
 
 router.route("/").get(getCourses).post(addCourse);
-router.route("/:id").get(getCourse);
+router.route("/:id").get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;

1f27d608763efc10403a305ee5352c70749b993c
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-10-13 08:52:21 +0530

    :boom: chore(Custom middleware for advanced Results)
    - To sort, filter, pagination

--- routes/bootcamps.js
+++ routes/bootcamps.js
@@ -9,6 +9,9 @@ const {
   bootcampPhotoUpload,
 } = require("../controller/bootcamps");
 
+const Bootcamp = require("../models/Bootcamp");
+const advancedResults = require("../middleware/advancedResults");
+
 // Include other resource routers
 const courseRouter = require("./courses");
 
@@ -19,7 +22,10 @@ router.use("/:bootcampId/courses", courseRouter);
 
 router.route("/radius/:zipcode/:distance").get(GetBootcampsInRadius);
 
-router.route("/").get(getBootcamps).post(createBootcamp);
+router
+  .route("/")
+  .get(advancedResults(Bootcamp, "courses"), getBootcamps)
+  .post(createBootcamp);
 
 router.route("/:id/photo").put(bootcampPhotoUpload);
 
--- routes/courses.js
+++ routes/courses.js
@@ -7,9 +7,21 @@ const {
   deleteCourse,
 } = require("../controller/courses");
 
+const Course = require("../models/Course");
+const advancedResults = require("../middleware/advancedResults");
+
 const router = express.Router({ mergeParams: true }); // We are merging the URL param
 
-router.route("/").get(getCourses).post(addCourse);
+router
+  .route("/")
+  .get(
+    advancedResults(Course, {
+      path: "bootcamp",
+      select: "name description",
+    }),
+    getCourses
+  )
+  .post(addCourse);
 router.route("/:id").get(getCourse).put(updateCourse).delete(deleteCourse);
 
 module.exports = router;

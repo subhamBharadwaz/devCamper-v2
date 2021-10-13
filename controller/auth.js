a85fd5f1b7e2dab7a4ddbe4166c0c2bb4dfd5b43
Subham Bharadwaz <subhamsbharadwaz@gmail.com>
2021-10-13 16:14:15 +0530

    :passport_control: auth(Sending JWt in a cookie)

--- controller/auth.js
+++ controller/auth.js
@@ -19,10 +19,7 @@ exports.register = asyncHandler(async (req, res, next) => {
     role,
   });
 
-  // Create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
 
 /**
@@ -53,8 +50,27 @@ exports.login = asyncHandler(async (req, res, next) => {
     return next(new ErrorResponse(`Invalid credentials`, 401));
   }
 
-  // Create token
-  const token = user.getSignedJwtToken();
-
-  res.status(200).json({ success: true, token });
+  sendTokenResponse(user, 200, res);
 });
+
+// Get token from model, create cookie and send response
+const sendTokenResponse = async (user, statusCode, res) => {
+  // Check if password matches
+  const token = await user.getSignedJwtToken();
+
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  if ((process.env.NODE_ENV = "production")) {
+    options.secure = true;
+  }
+
+  res
+    .status(statusCode)
+    .cookie("token", token, options)
+    .json({ success: true, token });
+};
